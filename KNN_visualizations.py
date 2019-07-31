eccdba314f8b095a958fea6e97e65a85458e5bd9
asbird <ptasinskiwojciech@gmail.com>
2019-07-31 15:57:04 +0200

    Visualization functions script

--- KNN_visualizations.py
+++ KNN_visualizations.py
@@ -1,22 +1,6 @@
 import numpy as np
 import pdb
 import matplotlib.pyplot as plt
-import time
-
-def main():
-    data = {'y':[[1,1],[1,2],[2,1],[1,0]], 'b':[[5,4],[5,5],[4,5],[6,1]]}
-    point = [1.5,4]
-
-    fig = plt.figure()
-    plt.scatter(point[0],point[1],c='g')
-    scatter_data_points(data)
-    draw_pt_connections(point,data)
-    scatter_data_points(data)
-
-    fig.clf()
-    fig.show()
-
-
 
 
 def scatter_data_points(data):
@@ -26,13 +10,7 @@ def scatter_data_points(data):
 
 
 def draw_pt_connections(point,data):
-    time.sleep(1)
+    # Connections from point to all of the points from data
     for xxd,yxd in data:
         plt.plot([point[0],xxd],[point[1],yxd], c='r', linewidth=1)
-        plt.pause(0.3)
-
-
-    # pdb.set_trace()
-
-if __name__ == '__main__':
-    main()
+        plt.pause(0.01)
